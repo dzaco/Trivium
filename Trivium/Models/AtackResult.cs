83c8bdccbd0a9af29fdaa6cebac05ee973287de5
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2022-10-05 22:15:44 +0200

    created empty attack view

---
+++ Trivium/Models/AtackResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Numerics;
+
+namespace Trivium.Models
+{
+    public class AtackResult
+    {
+        public BigInteger Id { get; set; }
+        public string EncryptedText { get; set; }
+        public DateTime Duration { get; set; }
+    }
+}
---
+++ Trivium/Models/BruteForce.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivium.Models
+{
+    public class BruteForce
+    {
+        private readonly string text;
+        private readonly string encyptedText;
+        private readonly Decryptor decryptor;
+        private readonly int bitLength;
+
+        public BruteForce(string text, string encyptedText, Decryptor decryptor)
+        {
+            this.text = text;
+            this.encyptedText = encyptedText;
+            this.decryptor = decryptor;
+        }
+
+        public IEnumerable<AtackResult> Atack()
+        {
+            foreach (var key in GetAllCombination())
+            {
+                var result = CreateResult();
+                yield return result;
+            }
+        }
+
+        private IEnumerable<BitArray> GetAllCombination()
+        {
+            throw new NotImplementedException();
+        }
+
+        private AtackResult CreateResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
